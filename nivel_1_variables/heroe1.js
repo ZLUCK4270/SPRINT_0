44379f0a87f95cece49b0fc23841f7f73790aad9
CesarDT-bit <dominguezcesar498@gmail.com>
2026-03-02 10:24:26 -0500

    Fase 1: Crear mi superhéroe (Deadpool)

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -1,18 +1,18 @@
 let nombreHeroe = "Deadpool";
 let poder = "Regeneración acelerada y romper la cuarta pared";
-let nivelEnergia = 100;
+let nivelEnergia = 90;
 let ciudad = "Nueva York";
 let enemigo = "Francis";
 
 console.log(
   nombreHeroe +
-    " tiene " +
-    nivelEnergia +
-    " de energía y está defendiendo " +
-    ciudad +
-    " de " +
-    enemigo +
-    " usando su poder de " +
-    poder +
-    ".",
+  " tiene " +
+  nivelEnergia +
+  " de energía y está defendiendo " +
+  ciudad +
+  " de " +
+  enemigo +
+  " usando su poder de " +
+  poder +
+  ".",
 );
